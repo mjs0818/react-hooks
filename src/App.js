8a2d71ceddc3904bb1cb7f85845129064b54122d
mjs0818 <tjfaudwo09@gmail.com>
2020-12-12 22:55:16 +0900

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import List from './List.jsx';
+import useFetch from './useFecth';
 
 const App = () => {
-  const [todos, setTodos] = useState(['js공부']);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState();
+
+  const loading = useFetch(setTodos, 'http://localhost:8080/todo');
 
   const changeInputData = (e) => {
     setNewTodo(e.target.value);
   };
   const addTodo = (e) => {
     e.preventDefault();
-    setTodos([...todos, newTodo]);
+    setTodos([...todos, { title: newTodo, id: todos.length, status: 'todo' }]);
   };
 
   useEffect(() => {
     console.log('새로운 내용이 렌더링됐네요', todos);
   }, [todos]);
+
   return (
     <>
       <h1>todo 애플리케이션</h1>
@@ -24,7 +28,7 @@ const App = () => {
         <input type="text" name="" onChange={changeInputData} />
         <button onClick={addTodo}>할일추가</button>
       </form>
-      <List todos={todos} />
+      <List todos={todos} loading={loading} />
     </>
   );
 };
---
+++ src/useFecth.js
@@ -0,0 +1,19 @@
+const { useState, useEffect } = require('react');
+
+const useFetch = (callback, url) => {
+  const [loading, setLoading] = useState(false);
+  const fetchInitialData = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const initialData = await response.json();
+    callback(initialData);
+    setLoading(false);
+  };
+  useEffect(() => {
+    console.log(1);
+    fetchInitialData();
+  }, []); // useEffect가 관찰할 대상이 [] (=null)이기 때문에 처음 한번만 실행이 됨
+  return loading;
+};
+
+export default useFetch;
