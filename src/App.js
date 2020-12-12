6456a6bd197a84d993501c623b7af60819ded6d0
mjs0818 <tjfaudwo09@gmail.com>
2020-12-12 22:23:45 +0900

    learn useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
+import List from './List.jsx';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState(['js공부']);
+  const [newTodo, setNewTodo] = useState();
+
+  const changeInputData = (e) => {
+    setNewTodo(e.target.value);
+  };
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  };
+
+  useEffect(() => {
+    console.log('새로운 내용이 렌더링됐네요', todos);
+  }, [todos]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>todo 애플리케이션</h1>
+      <form action="">
+        <input type="text" name="" onChange={changeInputData} />
+        <button onClick={addTodo}>할일추가</button>
+      </form>
+      <List todos={todos} />
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/List.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const List = ({ todos }) => {
+  const todoList = todos.map((todo) => <li>{todo}</li>);
+  return <ul>{todoList}</ul>;
+};
+
+export default List;
