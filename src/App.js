27955a2a76aa1bd4b284d2d7668229e530749cf6
mjs0818 <tjfaudwo09@gmail.com>
2020-12-12 23:14:14 +0900

    passing state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import List from './List.jsx';
 import useFetch from './useFecth';
-
+import Header from './Header';
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState();
@@ -17,18 +17,34 @@ const App = () => {
     setTodos([...todos, { title: newTodo, id: todos.length, status: 'todo' }]);
   };
 
+  const changeTodoStatus = (id) => {
+    const updateTodos = todos.map((todo) => {
+      if (todo.id === +id) {
+        if (todo.status === 'done') todo.status = 'todo';
+        else todo.status = 'done';
+      }
+      return todo;
+    });
+
+    setTodos(updateTodos);
+  };
   useEffect(() => {
     console.log('새로운 내용이 렌더링됐네요', todos);
   }, [todos]);
 
   return (
     <>
+      <Header todos={todos} />
       <h1>todo 애플리케이션</h1>
       <form action="">
         <input type="text" name="" onChange={changeInputData} />
         <button onClick={addTodo}>할일추가</button>
       </form>
-      <List todos={todos} loading={loading} />
+      <List
+        todos={todos}
+        loading={loading}
+        changeTodoStatus={changeTodoStatus}
+      />
     </>
   );
 };
--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Item from './Item.jsx';
 
-const List = ({ todos, loading }) => {
+const List = ({ todos, loading, changeTodoStatus }) => {
   let todoList = <div>loading...</div>;
   if (!loading)
-    todoList = todos.map((todo) => <li key={todo.id}>{todo.title}</li>);
+    todoList = todos.map((todo) => (
+      <Item key={todo.id} todo={todo} changeTodoStatus={changeTodoStatus} />
+    ));
   return <ul>{todoList}</ul>;
 };
 
